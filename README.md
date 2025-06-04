5d1999e406f1c1dd2d673a9d92d7451f75a18494
SoulHiro <victormts.s1@gmail.com>
2025-06-04 19:09:46 -0300

    First Commit!

---
+++ README.md
@@ -0,0 +1,6 @@
+### CheckList para a criação do Weather-App
+
+[] - Search Bar and ShowCase
+[] - Hourly Forecast and Details
+[] - Weekly Forecast
+[] - Responsiveness
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex max-w-6xl mx-auto justify-center items-center h-screen">
+      <h1 className="">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
